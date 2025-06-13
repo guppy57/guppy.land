615c937a5d32b6a23b86df1687a0083c39904513
Armaan Gupta <armaan@guppy.im>
2025-06-13 17:21:35 -0500

    ref: correct footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,14 +2,12 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <div>
-      <Image
-        src="/images/kreative-edgy-logo.svg"
-        alt="Edgy Kreative Logo in black"
-        className="w-full h-auto absolute bottom-0"
-        width={1000}
-        height={300}
-      />
-    </div>
+    <Image
+      src="/images/GUPPY-type-wide.svg"
+      alt="Guppy typographic logo wide in black"
+      className="w-full h-auto absolute bottom-0"
+      width={100}
+      height={300}
+    />
   )
 }
